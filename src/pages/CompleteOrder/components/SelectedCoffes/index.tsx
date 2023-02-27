c9d02544732f9ccf457d11701c5e6c2c4b3f26d0
Cauan Teixeira <cauantt007@gmail.com>
2023-02-27 10:12:53 -0300

    coffeeCardCart

--- src/pages/CompleteOrder/components/SelectedCoffes/index.tsx
+++ src/pages/CompleteOrder/components/SelectedCoffes/index.tsx
@@ -1,11 +1,17 @@
 import { SelectedCoffeesContainer, SelectedCoffeesContent } from "./styles";
+import { useCart } from "../../../../hooks/useCart"
 
 export function SelectedCoffees() {
+
+    const {cartItems } = useCart()
+
     return (
         <SelectedCoffeesContainer>
             <h4>Cafés selecionados</h4>
             <SelectedCoffeesContent>
-                
+                {cartItems.map((item) => (
+                    
+                ))}
             </SelectedCoffeesContent>
         </SelectedCoffeesContainer>
     )

3d5ceef08ee9c8632ae499b3336db75db21e2185
Cauan Teixeira <cauantt007@gmail.com>
2023-03-08 20:13:45 -0300

    OrderConfirmed styles

--- src/pages/OrderConfirmed/index.tsx
+++ src/pages/OrderConfirmed/index.tsx
@@ -1,6 +1,9 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { OrderData } from "../CompleteOrder";
-import { ConfirmedOrderContainer } from "./styles";
+import { ConfirmedOrderContainer, DetailsOrderContainer, DetailsOrderContent, ItemDetails } from "./styles";
+
+import deliveryBrand from "../../assets/delivery.svg"
+import { CurrencyDollarSimple, MapPin, Money, Timer } from "phosphor-react";
 
 interface LocationType {
     state: OrderData;
@@ -14,6 +17,40 @@ export function OrderConfirmed(){
         <ConfirmedOrderContainer className="container">
             <h2>Uhu! Pedido confirmado</h2>
             <span>Agora é só aguardar que logo o café chegará até você</span>
+
+            <DetailsOrderContainer>
+                <DetailsOrderContent>
+                    <ItemDetails>
+                        <div className="Icon" id="map">
+                            <MapPin weight="fill" color="#FFFF"/>
+                        </div>
+                        <div>
+                            <p>Entrega em {state.street}, {state.number}</p>
+                            <p>{state.city}, {state.uf}</p>
+                        </div>
+                    </ItemDetails>
+                    <ItemDetails>
+                        <div className="Icon" id="timer">
+                            <Timer weight="fill" color="#FFFF"/>
+                        </div>
+                        <div>
+                            <p>Previsão de entrega</p>
+                            <p>20 min - 30 min</p>
+                        </div>
+                    </ItemDetails>
+                    <ItemDetails>
+                        <div className="Icon" id="money">
+                            <CurrencyDollarSimple weight="fill" color="#FFFF"/>
+                        </div>
+                        <div>
+                            <p>Pagamento na entrega</p>
+                            <p>{}</p>
+                        </div>
+                    </ItemDetails>
+                    
+                </DetailsOrderContent>
+                <img src={deliveryBrand} alt="" />
+            </DetailsOrderContainer>
         </ConfirmedOrderContainer>
     )
 }
--- src/pages/OrderConfirmed/styles.ts
+++ src/pages/OrderConfirmed/styles.ts
@@ -2,6 +2,97 @@ import styled from "styled-components"
 
 export const ConfirmedOrderContainer = styled.div`
 
-    
+    padding-top: 5rem;
 
+    h2 {
+        color: #C47F17;
+        padding-bottom: 0.25rem;
+    }
+
+    span {
+        font-size: 20px;
+    }
+
+`
+
+export const DetailsOrderContainer = styled.div`
+
+    padding-top: 3rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 6rem;
+
+    width: 100%;
+
+
+`
+
+export const DetailsOrderContent = styled.div`
+
+    width: 100%;
+
+    padding: 2.5rem;
+
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+
+    border-radius: 6px 36px 6px 36px;
+    position: relative;
+    background: ${({ theme }) => theme.colors["base-background"]};
+    min-width: 32rem;
+
+    &::before {
+    content: "";
+    position: absolute;
+    inset: -1px;
+    z-index: -1;
+    border-radius: 7px 37px 7px 37px;
+    background: linear-gradient(102.89deg, #dbac2c 2.61%, #8047f8 98.76%);
+  }
+  
+    
+`
+
+export const ItemDetails = styled.div`
+
+
+    display: flex;
+
+    gap: 1rem;
+    font-size: 1rem;
+
+    min-width: 18rem;
+
+    p {
+        font-family: 'Roboto', sans-serif;
+        font-weight: 400;
+        font-size: 1rem;
+        color: #574F4D;
+    }
+
+    .Icon {
+        width: 2rem;
+        height: 2rem;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        border-radius: 999px;
+    }
+
+    #map {
+        background-color: #8047F8;
+    }
+
+    #timer {
+        background-color: #DBAC2C;
+    }
+
+    #money {
+        background-color: #C47F17;
+    }
 `
